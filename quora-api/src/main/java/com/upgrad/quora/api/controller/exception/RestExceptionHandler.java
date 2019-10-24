1e5509e46992cbcee9d16b9f8f5975b7b8ddd216
Sathwik ENVS <sathwik.edupuganti@gmail.com>
2019-10-24 23:07:00 +0530

    created Common Class

---
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/exception/RestExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.upgrad.quora.api.controller.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+
+}
---
+++ quora-service/src/main/java/com/upgrad/quora/service/common/dao/UserDao.java
@@ -0,0 +1,4 @@
+package com.upgrad.quora.service.common.dao;
+
+public class UserDao {
+}
