82af1fd2227957dadfb8a1cc20e3600637783500
Sathwik ENVS <sathwik.edupuganti@gmail.com>
2019-11-02 12:48:50 +0530

    Edit and delete endpoints added

--- quora-api/src/main/java/com/upgrad/quora/api/controller/exception/RestExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package com.upgrad.quora.api.controller.exception;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-
-@ControllerAdvice
-public class RestExceptionHandler {
-
-
-}
