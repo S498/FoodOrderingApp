996aafbc399192d85906524df441e5affc559326
Sathwik ENVS <sathwik.edupuganti@gmail.com>
2019-11-02 16:35:54 +0530

    Delete and Edit functionality added to answer endpoints

--- quora-service/src/main/java/com/upgrad/quora/service/common/EndPointIdentifier.java
+++ quora-service/src/main/java/com/upgrad/quora/service/common/EndPointIdentifier.java
@@ -1,7 +1,7 @@
 package com.upgrad.quora.service.common;
 
 /**
- * @author Manish Rout (https://github.com/s498)
+ * @author Manish Rout (https://github.com/mnshrt)
  * Description - Interface for constant used for identifying checkpoints
  */
 public interface EndPointIdentifier {
