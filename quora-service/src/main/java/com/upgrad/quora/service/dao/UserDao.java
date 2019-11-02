d6e3e9c9f271c0081119d79d058d5ee0f795cf58
Sathwik ENVS <sathwik@simbaquartz.com>
2019-11-02 12:58:12 +0530

    User Profile API and the endpoint added

--- quora-service/src/main/java/com/upgrad/quora/service/dao/UserDao.java
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/UserDao.java
@@ -3,19 +3,38 @@ package com.upgrad.quora.service.dao;
 import com.upgrad.quora.service.entity.UserAuthTokenEntity;
 import com.upgrad.quora.service.entity.UserEntity;
 import org.springframework.stereotype.Repository;
+
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 
-
 @Repository
 public class UserDao {
+
+
     @PersistenceContext
     private EntityManager entityManager;
 
     public UserEntity createUser(UserEntity userEntity) {
         entityManager.persist(userEntity);
         return userEntity;
+    }
+
+    public UserEntity getUser(final String userName) {
+        try {
+            return entityManager.createNamedQuery("userByUserame", UserEntity.class).setParameter("username", userName)
+                    .getSingleResult();
+        } catch (NoResultException nre) {
+            return null;
+        }
+    }
+
+    public UserEntity getUserByEmail(final String email) {
+        try {
+            return entityManager.createNamedQuery("userByEmail", UserEntity.class).setParameter("email", email).getSingleResult();
+        } catch (NoResultException nre) {
+            return null;
+        }
     }
 
     public UserAuthTokenEntity createAuthToken(final UserAuthTokenEntity userAuthTokenEntity) {
@@ -27,31 +46,6 @@ public class UserDao {
         entityManager.merge(updatedUserEntity);
     }
 
-    public UserEntity getUserByUserName(final String username) {
-        try {
-            return entityManager.createNamedQuery("userByUserName", UserEntity.class).setParameter("userName", username).getSingleResult();
-        } catch (NoResultException nre) {
-            return null;
-        }
-    }
-
-    public UserEntity getUserByUuid(final String uuid) {
-        try {
-            return entityManager.createNamedQuery("userByUuid", UserEntity.class).setParameter("uuid", uuid).getSingleResult();
-        } catch (NoResultException nre) {
-
-            return null;
-        }
-
-    }
-
-    public UserEntity getUserByEmail(final String email) {
-        try {
-            return entityManager.createNamedQuery("userByEmail", UserEntity.class).setParameter("email", email).getSingleResult();
-        } catch (NoResultException nre) {
-            return null;
-        }
-    }
 
     public UserAuthTokenEntity getUserAuthToken(final String accessToken) {
         try {
@@ -60,15 +54,7 @@ public class UserDao {
 
             return null;
         }
-    }
 
-    public UserAuthTokenEntity getUserAuthTokenByUuid(final String uuid){
-        try {
-            return entityManager.createNamedQuery("userAuthTokenByUuid", UserAuthTokenEntity.class).setParameter("uuid", uuid).getSingleResult();
-        } catch (NoResultException nre) {
-
-            return null;
-        }
     }
 
 }
--- quora-service/src/main/java/com/upgrad/quora/service/entity/UserAuthTokenEntity.java
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/UserAuthTokenEntity.java
@@ -4,8 +4,6 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -16,40 +14,39 @@ import java.time.ZonedDateTime;
 @Entity
 @Table(name = "user_auth")
 @NamedQueries({
-        @NamedQuery(name = "userAuthTokenByAccessToken", query = "select ut from UserAuthTokenEntity ut where ut.accessToken = :accessToken "),
-        @NamedQuery(name="UserByUserUuid",query ="select ut from UserAuthTokenEntity ut where ut.id =:uuid"),
-        @NamedQuery(name="userAuthTokenByUuid",query="select ut from UserAuthTokenEntity ut where ut.uuid = :uuid")
+        @NamedQuery(name = "userAuthTokenByAccessToken", query = "select ut from UserAuthTokenEntity ut where ut.accessToken = :accessToken ")
 })
 public class UserAuthTokenEntity implements Serializable {
 
+
     @Id
-    @Column(name = "ID")
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "UUID")
-    @Size(max = 200)
+    @Column(name = "uuid")
+    @NotNull
+    @Size(max = 64)
     private String uuid;
 
     @ManyToOne
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    @JoinColumn(name = "USER_ID")
-    private UserEntity user;
+    @JoinColumn(name = "user_id")
+    private UserEntity user_id;
 
-    @Column(name = "ACCESS_TOKEN")
+    @Column(name = "access_token")
     @NotNull
     @Size(max = 500)
     private String accessToken;
 
-    @Column(name = "LOGIN_AT")
+    @Column(name = "login_at")
     @NotNull
     private ZonedDateTime loginAt;
 
-    @Column(name = "EXPIRES_AT")
+    @Column(name = "expires_at")
     @NotNull
     private ZonedDateTime expiresAt;
 
-    @Column(name = "LOGOUT_AT")
+    @Column(name = "logout_at")
     private ZonedDateTime logoutAt;
 
     public Integer getId() {
@@ -68,12 +65,12 @@ public class UserAuthTokenEntity implements Serializable {
         this.uuid = uuid;
     }
 
-    public UserEntity getUser() {
-        return user;
+    public UserEntity getUser_id() {
+        return user_id;
     }
 
-    public void setUser(UserEntity user) {
-        this.user = user;
+    public void setUser_id(UserEntity user_id) {
+        this.user_id = user_id;
     }
 
     public String getAccessToken() {
@@ -107,6 +104,74 @@ public class UserAuthTokenEntity implements Serializable {
     public void setLogoutAt(ZonedDateTime logoutAt) {
         this.logoutAt = logoutAt;
     }
+/*
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public UserEntity getUser() {
+        return user;
+    }
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+    public String getAccessToken() {
+        return accessToken;
+    }
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+    public ZonedDateTime getLoginAt() {
+        return loginAt;
+    }
+    public void setLoginAt(ZonedDateTime loginAt) {
+        this.loginAt = loginAt;
+    }
+    public ZonedDateTime getExpiresAt() {
+        return expiresAt;
+    }
+    public void setExpiresAt(ZonedDateTime expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+    public ZonedDateTime getLogoutAt() {
+        return logoutAt;
+    }
+    public void setLogoutAt(ZonedDateTime logoutAt) {
+        this.logoutAt = logoutAt;
+    }
+    public Long getVersion() {
+        return version;
+    }
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+    public String getCreatedBy() {
+        return createdBy;
+    }
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+    public ZonedDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(ZonedDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+    public ZonedDateTime getModifiedAt() {
+        return modifiedAt;
+    }
+    public void setModifiedAt(ZonedDateTime modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+    */
 
     @Override
     public boolean equals(Object obj) {
